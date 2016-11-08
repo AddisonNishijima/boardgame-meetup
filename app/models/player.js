c5a5cc8cdd1ffeed097fd00bea893cca246e879c
Aimen Khakwani and Zachary Matthewstearn <aimen@>
2016-11-07 16:03:58 -0800

    add models

--- app/models/player.js
+++ app/models/player.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  name: DS.attr(),
+  username: DS.attr(),
+  password: DS.attr(),
+  games_bringing: DS.hasMany('game', {async: true}),
 });
