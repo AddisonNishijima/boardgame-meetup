6c521d2720d8c9471e43d287ceebd7c12348aeec
Aimen Khakwani and Ewa Manek <aimen+ewa@>
2016-11-08 10:36:41 -0800

    Add delete functionality to sessions

--- app/components/session-detail.js
+++ app/components/session-detail.js
@@ -4,6 +4,11 @@ export default Ember.Component.extend({
   actions: {
     updateForm(params, session) {
       this.sendAction('updateForm', params, session);
+    },
+    delete(session) {
+      if(confirm('Are you sure you want to delete this session?')) {
+        this.sendAction('delete', session);
+      }
     }
   }
 });
