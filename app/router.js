66a5e34d463c4a6e8d4e634bc37ed8c0ca97ede6
Aimen Khakwani and Ewa Manek <aimen+ewa@>
2016-11-08 11:27:36 -0800

    Add individual game componenet

--- app/router.js
+++ app/router.js
@@ -12,7 +12,9 @@ Router.map(function() {
   this.route('index', function() {
     this.route('session', {path: 'session/:session_id'});
   });
-  this.route('games');
+  this.route('games', function() {
+    this.route('game', {path: 'game/:game_id'});
+  });
   this.route('players', function() {
     this.route('player', {path: 'player/:player_id'});
   });
