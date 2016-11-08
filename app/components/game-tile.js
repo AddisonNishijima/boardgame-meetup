88ac4cc503905f44389a9a81ed24dd277930f6eb
Aimen Khakwani and Ewa Manek <aimen@>
2016-11-08 15:06:57 -0800

    Allow users to declare they own a game

--- app/components/game-tile.js
+++ app/components/game-tile.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  currentPlayer: Ember.inject.service(),
+  ownsGame: Ember.computed('currentPlayer.curPlayer.games_owned', 'game', function(){
+    var doesOwn= false;
+    var game = this.get('game');
+    var games = this.get('currentPlayer.curPlayer.games_owned');
+    if(games){
+      games.forEach(function(g){
+        if(g.id === game.id){
+          doesOwn = true;
+        }
+      });
+    }
+    return doesOwn;
+  }),
+  actions: {
+    ownGame(game){
+      this.sendAction('ownGame', game);
+    }
+  }
 });
--- app/routes/games.js
+++ app/routes/games.js
@@ -1,7 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  currentPlayer: Ember.inject.service(),
   model() {
     return this.store.findAll('game');
+  },
+  actions: {
+    ownGame(game){
+      var player = this.get('currentPlayer.curPlayer');
+      game.get('owners').addObject(player);
+      player.get('games_owned').addObject(game);
+      game.save().then(function(){
+        return player.save();
+      });
+    },
+    lostGame(game){
+      var player = this.get('currentPlayer.curPlayer');
+      game.set('owner', player);
+      player.get('games_owned').addObject(game);
+      game.save().then(function(){
+        return player.save();
+      });
+    }
   }
 });
