35ee5b274d947301b2e1b4d6ffa40bea69e0161d
Addison Nishijima and Zachary Matthewstearn <addison+zachary@>
2016-11-09 11:40:03 -0800

    add css styling for login panel, add blank avatar image

--- app/models/player.js
+++ app/models/player.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   name: DS.attr(),
   username: DS.attr(),
   password: DS.attr(),
+  avatar: DS.attr(),
   games_owned: DS.hasMany('game', {async: true}),
   sessions: DS.hasMany('session', {async: true})
 });
