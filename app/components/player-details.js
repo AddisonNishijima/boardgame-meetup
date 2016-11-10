d53350696c272a175bedb0f6eb313ada111f21ae
Aimen Khakwani and Ewa Manek <aimen+ewa@>
2016-11-09 16:43:40 -0800

    Update readme with image

--- app/components/player-details.js
+++ app/components/player-details.js
@@ -16,8 +16,9 @@ export default Ember.Component.extend({
       if(confirm("Are you sure you want to permanently terminate your membership?")) {
         if(confirm("Really are you sure tho?")){
           if(confirm("Cos this is pretty permanent. Don't you love us?")){
+            this.get('currentPlayer').logout();
             this.sendAction('deletePlayer', player);
-          }  
+          }
         }
       }
     }
